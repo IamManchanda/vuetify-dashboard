88c0dc44c5a9921792d9bc8c48f8d88c224aaff4
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-01-30 22:10:11 +0530

    Login form with Vuetify!

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,59 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld />
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card width="500" class="mx-auto mt-6 pa-4">
+              <v-card-title>
+                <h1 class="display-1">Login</h1>
+              </v-card-title>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="Username"
+                    prepend-icon="mdi-account-circle"
+                  />
+                  <v-text-field
+                    :type="passwordEyeType"
+                    label="Password"
+                    prepend-icon="mdi-lock"
+                    :append-icon="passwordEyeIcon"
+                    @click:append="handlePasswordEyeToggle"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn color="success">Register</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="info">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-
-  components: {
-    HelloWorld
-  },
-
   data: () => ({
-    //
-  })
+    showPasswords: true,
+  }),
+  computed: {
+    passwordEyeType() {
+      return this.showPasswords ? "password" : "text";
+    },
+    passwordEyeIcon() {
+      return this.showPasswords ? "mdi-eye" : "mdi-eye-off";
+    },
+  },
+  methods: {
+    handlePasswordEyeToggle() {
+      this.showPasswords = !this.showPasswords;
+    },
+  },
 };
 </script>
