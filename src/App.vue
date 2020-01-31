15412da88e396d9aaec6d14586d0507be8af32c1
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-01-31 19:09:25 +0530

    Working pass on login page - UI Design!

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,74 @@
 <template>
   <v-app>
+    <v-app-bar color="primary lighten-1" app dark>
+      <v-toolbar-title class="ms-4">Vuetify Dashboard</v-toolbar-title>
+      <v-spacer />
+      <v-btn text rounded class="me-4">
+        Home
+      </v-btn>
+      <v-btn text rounded class="me-4">
+        Login
+      </v-btn>
+    </v-app-bar>
     <v-content>
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4">
-            <v-card width="500" class="mx-auto mt-6 pa-4">
-              <v-card-title>
-                <h1 class="display-1">Login</h1>
-              </v-card-title>
-              <v-card-text>
-                <v-form>
-                  <v-text-field
-                    label="Username"
-                    prepend-icon="mdi-account-circle"
-                  />
-                  <v-text-field
-                    :type="passwordEyeType"
-                    label="Password"
-                    prepend-icon="mdi-lock"
-                    :append-icon="passwordEyeIcon"
-                    @click:append="handlePasswordEyeToggle"
-                  />
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-btn color="success">Register</v-btn>
-                <v-spacer></v-spacer>
-                <v-btn color="info">Login</v-btn>
-              </v-card-actions>
+          <v-col cols="12" sm="8" md="6" lg="4">
+            <v-card class="mx-auto mt-6">
+              <v-toolbar
+                class="d-flex justify-center"
+                color="primary lighten-1"
+                dark
+                flat
+              >
+                <v-toolbar-title>
+                  <h1 class="headline">Login</h1>
+                </v-toolbar-title>
+              </v-toolbar>
+              <div class="pa-4">
+                <v-card-text>
+                  <v-form>
+                    <v-text-field
+                      label="Username"
+                      prepend-icon="mdi-account-circle"
+                    />
+                    <v-text-field
+                      :type="passwordEyeType"
+                      label="Password"
+                      prepend-icon="mdi-lock"
+                      :append-icon="passwordEyeIcon"
+                      @click:append="handlePasswordEyeToggle"
+                    />
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                  <v-btn color="success">Register</v-btn>
+                  <v-spacer></v-spacer>
+                  <v-btn color="info">Login</v-btn>
+                </v-card-actions>
+              </div>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
     </v-content>
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
@@ -41,6 +77,7 @@ export default {
   name: "App",
   data: () => ({
     showPasswords: true,
+    links: ["Home", "Login"],
   }),
   computed: {
     passwordEyeType() {
