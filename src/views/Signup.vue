c302f0d8f33ac0793655e339dcf1f582b06bb29f
Harry Manchanda <harmanmanchanda182@gmail.com>
2020-02-03 01:06:57 +0530

    Add validations!

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -9,19 +9,40 @@
             </v-toolbar-title>
           </v-toolbar>
           <div class="pa-4">
-            <v-form>
+            <v-form @submit.prevent="submit">
               <v-card-text>
                 <v-text-field
+                  v-model="username"
+                  :counter="10"
+                  label="Username"
+                  color="primary"
+                  prepend-icon="mdi-account-circle"
+                  :error-messages="username_errors"
+                  required
+                  @input="$v.username.$touch()"
+                  @blur="$v.username.$touch()"
+                />
+                <v-text-field
+                  v-model="email"
                   label="Email"
                   type="email"
                   color="primary"
                   prepend-icon="mdi-email"
+                  :error-messages="email_errors"
+                  required
+                  @input="$v.email.$touch()"
+                  @blur="$v.email.$touch()"
                 />
                 <v-autocomplete
+                  v-model="selected_browser"
                   label="Which Browser do you use"
                   color="primary"
                   prepend-icon="mdi-edge"
                   :items="browsers"
+                  :error-messages="selected_browser_errors"
+                  required
+                  @change="$v.selected_browser.$touch()"
+                  @blur="$v.selected_browser.$touch()"
                 />
                 <v-file-input label="Attach Profile Picture" color="primary" />
                 <v-dialog
@@ -50,13 +71,21 @@
                   />
                 </v-dialog>
                 <v-checkbox
+                  v-model="tnc"
                   label="Agree with terms and conditions"
                   color="primary"
+                  :error-messages="tnc_errors"
+                  required
+                  @change="$v.tnc.$touch()"
+                  @blur="$v.tnc.$touch()"
                 />
               </v-card-text>
               <v-card-actions class="d-flex justify-center">
                 <div class="my-2">
-                  <v-btn type="submit" color="primary" dark large>Submit</v-btn>
+                  <v-btn type="submit" color="primary" class="me-4" dark large>
+                    Submit
+                  </v-btn>
+                  <v-btn @click="clear" dark large>Clear</v-btn>
                 </div>
               </v-card-actions>
             </v-form>
@@ -68,14 +97,71 @@
 </template>
 
 <script>
+import { validationMixin } from "vuelidate";
+import { required, maxLength, email } from "vuelidate/lib/validators";
+
 export default {
   name: "Signup",
+  mixins: [validationMixin],
   data: () => ({
-    browsers: ["Chrome", "Safari", "Firefox", "Edge", "Opera", "Brave"],
     date: new Date().toISOString().substr(0, 10),
     menu: false,
+    browsers: ["Chrome", "Safari", "Firefox", "Edge", "Opera", "Brave"],
+    username: "",
+    email: "",
+    selected_browser: null,
+    tnc: "",
   }),
-  computed: {},
-  methods: {},
+  validations: {
+    username: { required, maxLength: maxLength(10) },
+    email: { required, email },
+    selected_browser: { required },
+    tnc: {
+      checked(val) {
+        return val;
+      },
+    },
+  },
+  computed: {
+    username_errors() {
+      const errors = [];
+      if (!this.$v.username.$dirty) return errors;
+      !this.$v.username.maxLength &&
+        errors.push("Username must be at most 10 characters long");
+      !this.$v.username.required && errors.push("Username is required.");
+      return errors;
+    },
+    email_errors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("Must be valid e-mail");
+      !this.$v.email.required && errors.push("E-mail is required");
+      return errors;
+    },
+    selected_browser_errors() {
+      const errors = [];
+      if (!this.$v.selected_browser.$dirty) return errors;
+      !this.$v.selected_browser.required && errors.push("Browser is required");
+      return errors;
+    },
+    tnc_errors() {
+      const errors = [];
+      if (!this.$v.tnc.$dirty) return errors;
+      !this.$v.tnc.checked && errors.push("You must agree to continue!");
+      return errors;
+    },
+  },
+  methods: {
+    submit() {
+      this.$v.$touch();
+    },
+    clear() {
+      this.$v.$reset();
+      this.username = "";
+      this.email = "";
+      this.selected_browser = null;
+      this.tnc = false;
+    },
+  },
 };
 </script>
